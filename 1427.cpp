1c8d85d95236ade2ef9079bc7a6b5c6455262e59
Suyeon Shin <suyeons5536@gmail.com>
2023-08-17 20:31:14 +0900

    23.08.16

--- 1427.cpp
+++ 1427.cpp
@@ -12,11 +12,14 @@ int main()
 
     int* number = new int[s.length()];
 
+    //string -> int
     for(int i = 0; i < s.length(); i++)
         number[i] = s[i] - 48;
-
+    
+    //내림차순 정렬
     sort(number, number + s.length(), greater<>());
 
+    //출력
     for(int i = 0; i < s.length(); i++)
         cout << number[i];
     cout << endl;
