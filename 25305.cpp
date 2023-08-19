000fe45baa3a7bdad0568b756928fa5bc3f93a6c
Suyeon Shin <suyeons5536@gmail.com>
2023-08-19 14:14:09 +0900

    23.08.19

--- 25305.cpp
+++ 25305.cpp
@@ -11,7 +11,9 @@ int main()
     for(int i = 0; i < n; i++)//성적 입력 받기
         cin >> scores[i];
     
-    sort(scores, scores + n);
+    sort(scores, scores + n);//오름차순 정렬
 
     cout << scores[n - k] << endl;
+
+    return 0;
 }
