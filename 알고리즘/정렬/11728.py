8ea18f365d524867c1fd346195710c47698b221a
SuyeonShinnnn <suyeons5536@gmail.com>
2024-10-31 16:00:58 +0900

    배열 합치기

---
+++ 알고리즘/정렬/11728.py
@@ -0,0 +1,10 @@
+# 배열 합치기
+
+n, m = map(int, input().split(' '))
+
+a = list(map(int, input().split(' ')))
+b = list(map(int, input().split(' ')))
+
+c = sorted(a + b)
+for i in c:
+    print(i, end = ' ')
