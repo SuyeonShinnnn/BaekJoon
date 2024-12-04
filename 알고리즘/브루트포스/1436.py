d5f74a55123f1f593a36f7df9e3d3c980aef156a
SuyeonShinnnn <suyeons5536@gmail.com>
2024-12-04 20:47:04 +0900

    영화감독 숌

---
+++ 알고리즘/브루트포스/1436.py
@@ -0,0 +1,16 @@
+# 영화감독 숌
+
+n = int(input())
+count = 0
+result = 666
+
+while True:
+    if '666' in str(result):
+        count += 1
+
+    if n == count:
+        break
+    
+    result += 1
+
+print(result)
