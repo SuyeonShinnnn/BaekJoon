76ece394bea10f6e16f9217ec246dcee042ad77a
SuyeonShinnnn <suyeons5536@gmail.com>
2024-12-08 22:18:46 +0900

    설탕 배달

---
+++ 알고리즘/DP/2839.py
@@ -0,0 +1,20 @@
+# 설탕 배달
+
+n = int(input())
+count = 0
+
+if n % 5 == 0:
+    count = n // 5
+else:
+    while n > 0:
+        n -= 3
+        count += 1
+
+        if n % 5 == 0:
+            count += n // 5
+            break
+        elif n == 1 or n == 2:
+            count = -1
+            break
+
+print(count)
